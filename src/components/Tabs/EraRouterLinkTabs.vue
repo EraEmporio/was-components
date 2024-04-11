83d6d9e8aaf2829675063a441370b5702820fc57
Mariana Castro <fercastro.mariana@gmail.com>
2024-04-11 18:52:19 -0300

    configuração e estilização do EraRouterLinkTabs (SH3 #38)

--- src/components/Tabs/EraRouterLinkTabs.vue
+++ src/components/Tabs/EraRouterLinkTabs.vue
@@ -1,18 +1,52 @@
 <template>
-
-    <div class="card">
-        <TabView :scrollable="true">
-            <TabPanel v-for="tab in scrollableTabs" :key="tab.title" :header="tab.title">
-                <p class="m-0">{{ tab.content }}</p>
-            </TabPanel>
-        </TabView>
-    </div>
+  <div class="card">
+    <TabView
+      :scrollable="true"
+      v-bind="$attrs"
+      @tab-change="
+        (e) => {
+          emits('changeRoute', tabGroup[e.index].name);
+        }
+      "
+    >
+      <TabPanel
+        v-for="(tab, index) in tabGroup"
+        v-bind:key="index"
+        :header="tab.label"
+      >
+        <slot></slot>
+      </TabPanel>
+    </TabView>
+  </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-import TabView from 'primevue/tabview';
-import TabPanel from 'primevue/tabpanel';
+<script setup lang="ts">
+import { PropType } from "vue";
+import TabView from "primevue/tabview";
+import TabPanel from "primevue/tabpanel";
 
-const scrollableTabs = ref(Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content` })));
+type RouterTab = {
+  name: string;
+  label: string;
+};
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+defineProps({
+  tabGroup: {
+    type: Array as PropType<Array<RouterTab>>,
+    default: () => <RouterTab[]>[],
+  },
+});
+
+/* const scrollableTabs = ref(
+  Array.from({ length: 50 }, (_, i) => ({
+    title: `Tab ${i + 1}`,
+    content: `Tab ${i + 1} Content`,
+  }))
+); */
+
+const emits = defineEmits(["changeRoute"]);
 </script>
--- src/presets/was/tabview/index.js
+++ src/presets/was/tabview/index.js
@@ -3,6 +3,7 @@ export default {
         class: [
             // Position
             'relative',
+            '-mb-3',
 
             // Misc
             { 'overflow-hidden': props.scrollable }
@@ -29,11 +30,11 @@ export default {
 
             // Size and Shape
             'h-full w-12',
-            'rounded-none',
+            'rounded-full',
 
             // Colors
             'bg-mercury-0 dark:bg-mercury-800',
-            'text-selenium-500 dark:text-selenium-400',
+            'text-selenium-500 hover:text-selenium-700 dark:text-selenium-400',
             'shadow-md'
         ]
     },
@@ -49,11 +50,11 @@ export default {
 
             // Size and Shape
             'h-full w-12',
-            'rounded-none',
+            'rounded-full',
 
             // Colors
             'bg-mercury-0 dark:bg-mercury-800',
-            'text-selenium-500 dark:text-selenium-400',
+            'text-selenium-500 hover:text-selenium-700 dark:text-selenium-400',
             'shadow-md'
         ]
     },
@@ -63,18 +64,20 @@ export default {
             'flex flex-1',
 
             // Spacing
+            'space-x-1',
             'list-none',
             'p-0 m-0',
 
             // Colors
             'bg-mercury-0 dark:bg-mercury-800',
-            'border-b border-mercury-200 dark:border-mercury-700',
             'text-mercury-900 dark:text-mercury-0/80'
         ]
     },
     tabpanel: {
         header: ({ props }) => ({
             class: [
+                
+
                 // Spacing
                 'mr-0',
 
@@ -88,38 +91,38 @@ export default {
             class: [
                 'relative',
 
+                //Size and shape
+                'min-w-[200px]',
+                'rounded-t-xl',
+
                 // Font
                 'font-medium',
                 'text-md',
 
                 // Flexbox and Alignment
-                'flex items-center',
+                'flex items-center justify-center',
 
                 // Spacing
-                'py-4 px-3',
-                '-mb-[1px]',
-
-                // Shape
-                'border-b-2',
-                'rounded-t-md',
+                'pt-3 pb-4 px-4',
+                
 
                 // Colors and Conditions
                 {
-                    'border-mercury-200 dark:border-mercury-700': parent.state.d_activeIndex !== context.index,
-                    'bg-mercury-0 dark:bg-mercury-800': parent.state.d_activeIndex !== context.index,
-                    'text-mercury-700 dark:text-mercury-0/80': parent.state.d_activeIndex !== context.index,
+                    'border-mercury-0 dark:border-mercury-700': parent.state.d_activeIndex !== context.index,
+                    'bg-mercury-100 dark:bg-mercury-800': parent.state.d_activeIndex !== context.index,
+                    'text-mercury-800 dark:text-mercury-0/80': parent.state.d_activeIndex !== context.index,
 
-                    'bg-mercury-0 dark:bg-mercury-800': parent.state.d_activeIndex === context.index,
-                    'border-selenium-500 dark:border-selenium-400': parent.state.d_activeIndex === context.index,
-                    'text-selenium-500 dark:text-selenium-400': parent.state.d_activeIndex === context.index
+                    'bg-white dark:bg-mercury-800': parent.state.d_activeIndex === context.index,
+                    'border-selenium-0 dark:border-selenium-400': parent.state.d_activeIndex === context.index,
+                    'text-mercury-950 dark:text-selenium-400': parent.state.d_activeIndex === context.index
                 },
 
                 // States
-                'focus-visible:outline-none focus-visible:outline-offset-0 focus-visible:ring-2 focus-visible:ring-inset',
-                'focus-visible:ring-selenium-500 dark:focus-visible:ring-selenium-400',
+                /* 'focus-visible:outline-none focus-visible:outline-offset-0 focus-visible:ring-2 focus-visible:ring-inset',
+                'focus-visible:ring-selenium-500 dark:focus-visible:ring-selenium-400', */
                 {
-                    'hover:bg-mercury-0 dark:hover:bg-mercury-800/80': parent.state.d_activeIndex !== context.index,
-                    'hover:border-mercury-400 dark:hover:border-mercury-600': parent.state.d_activeIndex !== context.index,
+                    'hover:bg-mercury-200 dark:hover:bg-mercury-800/80': parent.state.d_activeIndex !== context.index,
+                    'hover:border-mercury-0 dark:hover:border-mercury-600': parent.state.d_activeIndex !== context.index,
                     'hover:text-mercury-900 dark:hover:text-mercury-0': parent.state.d_activeIndex !== context.index
                 },
 
@@ -143,7 +146,7 @@ export default {
         content: {
             class: [
                 // Spacing
-                'p-6',
+                'pb-0',
 
                 // Shape
                 'rounded-b-md',
