847ff4ea0edaaa7506d5418516151cd4ce49504c
Barbara Boehchat <barbs.boechat@gmail.com>
2024-04-13 11:13:12 -0300

    criação da tabela com filtros personalizados

--- src/components/Tables/EraDataTable.vue
+++ src/components/Tables/EraDataTable.vue
@@ -1,14 +1,43 @@
 <template>
-  <DataTable :value="items" v-bind="$attrs">
+  <DataTable v-model:filters="filters" :value="items">
     <Column
       v-for="col of columns"
       :key="col.field"
       :field="col.field"
       :header="col.header"
-      v-bind="col.props"
+      v-bind="{ ...col.props }"
     >
       <template #body="slotProps" v-if="col.type == 'tag'">
-        <Tag :value="slotProps.data[col.field]" v-bind="col.props.tag" />
+        <Tag
+          :value="slotProps.data[col.field]"
+          v-bind="col.props.tag ? col.props.tag(slotProps.data) : {}"
+        />
+      </template>
+
+      <template #filter="{ filterModel, filterCallback }" v-if="col.filter">
+        <component
+          :is="filterComponents[col.filter.type]"
+          v-model="filterModel.value"
+          :filterCallback="filterCallback"
+          :col="col"
+        />
+      </template>
+    </Column>
+
+    <Column header="Ações" v-if="actions.length > 0" :exportable="false">
+      <template #body="slotProps">
+        <div class="actions-wrapper">
+          <Button
+            v-for="{ icon, color, action, disabled } of actions.filter(
+              (action) => action.permission(slotProps.data)
+            )"
+            :disabled="disabled(slotProps.data)"
+            @click="action(slotProps.data)"
+            :key="icon"
+          >
+            <Icon :icon="icon" :class="twMerge('w-5 h-auto text-sm', color)" />
+          </Button>
+        </div>
       </template>
     </Column>
   </DataTable>
@@ -18,17 +47,42 @@
 import DataTable from "primevue/datatable";
 import Column, { ColumnProps } from "primevue/column";
 import Tag, { TagProps } from "primevue/tag";
+import { Icon } from "@iconify/vue";
+import { PropType, markRaw } from "vue";
+import { twMerge } from "tailwind-merge";
+import { FilterMatchMode, FilterOperator } from "primevue/api";
+import SelectFilterTag from "./Filters/SelectFilterTag.vue";
+import { useAttrs } from "vue";
+import { useFilterTable } from "./Filters/useFilterTable";
 
-import { PropType } from "vue";
+const attrs = useAttrs();
 
-type ItemColum = {
+const filterComponents = {
+  SelectFilterTag: markRaw(SelectFilterTag),
+};
+
+export type ItemColum = {
   field: string;
   header: string;
   type?: "tag" | "download" | "actions";
-  props: ColumnProps & { tag?: TagProps };
+  props: ColumnProps & { tag?: (item: any) => TagProps };
+  filter?: {
+    operator: keyof typeof FilterOperator;
+    matchMode: keyof typeof FilterMatchMode;
+    type: keyof typeof filterComponents;
+    options: any[];
+  };
 };
 
-defineProps({
+type Action = {
+  color: string;
+  icon: string;
+  action: (item: any) => void;
+  permission: (item: any) => boolean | true;
+  disabled: (item: any) => boolean | false;
+};
+
+const props = defineProps({
   items: {
     type: Array as PropType<Array<any>>,
     default: () => <any[]>[],
@@ -37,9 +91,15 @@ defineProps({
     type: Array as PropType<Array<ItemColum>>,
     default: () => <ItemColum[]>[],
   },
+  actions: {
+    type: Array as PropType<Array<Action>>,
+    default: () => <Action[]>[],
+  },
 });
 
 defineOptions({
   inheritAttrs: true,
 });
+
+const { filters } = useFilterTable(attrs.filterDisplay, props.columns);
 </script>
---
+++ src/components/Tables/Filters/SelectFilterTag.vue
@@ -0,0 +1,46 @@
+<template>
+  <Dropdown
+    v-model="filterModel"
+    @change="filterCallback()"
+    :options="[...col.filter.options]"
+    placeholder="Selecione um"
+    class="p-column-filter"
+    optionLabel="name"
+    optionValue="value"
+    :showClear="true"
+  >
+    <template #option="slotProps">
+      <Tag
+        :value="slotProps.option.name"
+        :severity="slotProps.option.severity"
+      />
+    </template>
+  </Dropdown>
+</template>
+
+<script lang="ts" setup>
+import { ColumnFilterModelType } from "primevue/column";
+import Dropdown from "primevue/dropdown";
+import Tag from "primevue/tag";
+
+defineProps({
+  col: {
+    type: Object,
+    default: () => {
+      return {
+        filter: {
+          options: [],
+        },
+      };
+    },
+  },
+  filterCallback: {
+    type: Function,
+    default: () => {
+      // Implement your filter callback logic here
+    },
+  },
+});
+
+const filterModel = defineModel<ColumnFilterModelType>();
+</script>
