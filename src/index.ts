a3dc5b826dbc4c691f2f534fe3149b68d03fd9ea
Niehaus <barbs.boechat@gmail.com>
2024-06-07 00:49:55 -0300

    feat: Finalmente! Ajuste no bug dos presets não sendo totalmente carregados

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import PrimeVue from 'primevue/config';
 import Tooltip from "primevue/tooltip";
 import Was from './presets/was';
 import ptBrLocale from './utils/primeLocalePtBr';
-
+import { usePrimeVue } from "primevue/config";
 import * as components from './components'
 import { App } from 'vue'
 
-export default function install(app: App) {
+export default function install(app: App, options: any) {
   app.directive("tooltip", Tooltip);
   app.use(PrimeVue, {
     unstyled: true,
@@ -16,8 +16,16 @@ export default function install(app: App) {
     ripple: true,
     locale: {
       ...ptBrLocale
-    }
+    },
   });
+
+  app.mixin({
+    created() {
+      const primevue = usePrimeVue();
+      primevue.config.pt = Was;
+    },
+  });
+
   for (const key in components) {
     // @ts-expect-error
     app.component(key, components[key])
